b932908d4614437c4baf39c23be7ff260144e432
tlodge <tlodge@gmail.com>
2015-09-02 17:21:33 +0100

    cp commit

--- register/js/components/Occupancy.react.js
+++ register/js/components/Occupancy.react.js
@@ -24,28 +24,19 @@ var Occupancy = React.createClass({
       textAlign: 'center'
     };
    
-    return (
-
-        <form>
-         <div className="row">
-            <div className="large-12 columns unpadded">
-              <div className="cell">
-                <div className="row">
-                  <div className="small-4 large-4 columns">
-                    <div style={apartment}>459</div>
-                    <div style={block}> block 35 </div>
-                  </div>
-                  <div className="small-8 large-8 columns">
-                       <ul className="button-group">
-                          <li style={listyle}><div style={greenback} className="button tiny">owner</div></li>
-                          <li style={listyle}><div style={greenback} className="button tiny">tenant</div></li>
-                          <li style={listyle}><div style={greenback} className="button tiny">own and occupy</div></li>
-                      </ul>
-                  </div>
-                </div>
-              </div>
+    return (<form>
+        <div className="row">
+          <div className="large-12 columns unpadded">
+            <div className="cell">
+              <label>4534 charthouse</label>  
+              <ul className="button-group">
+                    <li style={listyle}><div style={greenback} className="button tiny">owner</div></li>
+                    <li style={listyle}><div style={greenback} className="button tiny">tenant</div></li>
+                    <li style={listyle}><div style={greenback} className="button tiny">own and occupy</div></li>
+              </ul>
             </div>
           </div>
+        </div>
       </form>);
   }
 });
